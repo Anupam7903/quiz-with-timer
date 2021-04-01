f1d7b73055c66c0062b7c5da91b3771d6c91640f
AdarshSingh223 <76893885+AdarshSingh223@users.noreply.github.com>
2021-04-01 15:33:21 +0530

    Update project.c

--- project.c
+++ project.c
@@ -2,14 +2,13 @@
 #include<stdlib.h>
 #include<windows.h>
 #include<string.h>
-//#include"qmaker.h"
 
 #define TRUE 1
-#define MAX_TIME 30
+#define MAX_TIME 7
 
-//char * qm(int a);
 void showoff(int t);
 void fileprint(char file_name[25]);
+void endscreen(int timee);
 
 int main(void)
 {
@@ -17,7 +16,7 @@ int main(void)
     int i,min = 00, pass_time = 5;
     char A[25][50]={"question1.txt","question2.txt","question3.txt","question4.txt","question5.txt",
                     "question6.txt","question7.txt","question8.txt","question9.txt","question10.txt",
-                    "question11.txt","question12.txt","question13.txt","question14.txt","question15.txt",
+                    "question11.txt","question12.txt","question13.txt","question14.txt","question15.txt",           //to work
                     "question16.txt","question17.txt","question18.txt","question19.txt","question20.txt",
                     "question21.txt","question22.txt","question23.txt","question24.txt","question25.txt",
                     "question26.txt","question27.txt","question28.txt","question29.txt","question30.txt"};
@@ -25,7 +24,7 @@ int main(void)
     FILE *fp;
     char c,file_name[30],ans = '^';
 
-    for(i = 0 ; i < 30 ; ++i)
+    for(i = 0 ; i < 30 ; ++i)               //to work
     {
     while(TRUE)
     {
@@ -36,43 +35,16 @@ int main(void)
         printf("\n");
        
         time_left--;
-       
+
         fileprint(file_name);
-        /*fp = fopen(file_name,"r");
-       
-        while(TRUE)
-        {
-             c = fgetc(fp);
-             if(c == EOF)
-             break;
-       
-             if(c == '$')            //for new line
-             printf("\n");
-             else
-             printf("%c",c);
-        }*/
-       /* if(time_left != -1)
-        {
-            printf("\nYour answer : ");
-            scanf("%c",&ans);
-        }*/
+        
         Sleep(1000);
         
         if(time_left == -1)
         {
-            while(pass_time != 0)
-            {
-                system("cls");
-                printf("\n\n\n\t\t\t\t\t\t         TIME UP!");
-                if(pass_time > 1)
-                printf("\n\t\t\t\t\t\tNEXT QUESTION IN %d SECONDS",pass_time);
-                else
-                printf("\n\t\t\t\t\t\tNEXT QUESTION IN %d SECOND",pass_time);
-
-                pass_time--;
-                Sleep(1000);
-            }
+            endscreen(pass_time);
             pass_time = 5;
+          
             break;
         }
     } 
@@ -80,20 +52,6 @@ int main(void)
     }
     return 0;
 }
-
-/*char * qm(int a)
-{
-    char b[25];
-    char A[25][50]={"question1.txt","question2.txt","question3.txt","question4.txt","question5.txt",
-                    "question6.txt","question7.txt","question8.txt","question9.txt","question10.txt",
-                    "question11.txt","question12.txt","question13.txt","question14.txt","question15.txt",
-                    "question16.txt","question17.txt","question18.txt","question19.txt","question20.txt",
-                    "question21.txt","question22.txt","question23.txt","question24.txt","question25.txt",
-                    "question26.txt","question27.txt","question28.txt","question29.txt","question30.txt"};
-
-    strcpy(b,A[a]);
-    return A[a];
-}*/
 
 void showoff(int t)
 {
@@ -138,3 +96,33 @@ void fileprint(char file_name[25])
         }
 
 }
+
+void endscreen(int timee)
+{
+     while(timee != 0)
+            {
+                system("cls");
+                printf("\n\n\n\t\t\t\t\t\t         TIME UP!");
+                if(timee > 1)
+                printf("\n\t\t\t\t\t\tNEXT QUESTION IN %d SECONDS",timee);
+                else
+                printf("\n\t\t\t\t\t\tNEXT QUESTION IN %d SECOND",timee);
+
+                timee--;
+                Sleep(1000);
+            }
+}
+
+
+
+/*  PROBLEMS :
+
+    scanf problem (ip op)
+    int to string problem (question automation`)
+    student database problem(to make)
+    rand and srand (to make)
+    blinking problem
+    timer problem (00:00)
+    answers (check)
+
+*/    
